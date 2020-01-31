fd497788924f759c78b9eb12087b1ed89063ca89
ZhenfeiYu <55257370+ZhenfeiYu@users.noreply.github.com>
2020-01-31 15:44:27 -0500

    Add files via upload

---
+++ test_arabic2roman.py
@@ -0,0 +1,13 @@
+import arabic2roman as ar
+def test_ar():
+	assert ar.intToRoman(1)=="I"
+	assert ar.intToRoman(4)=="IV"
+	assert ar.intToRoman(9)=="IX"
+	assert ar.intToRoman(16)=="XVI"
+	assert ar.intToRoman(25)=="XXV"
+	assert ar.intToRoman(36)=="XXXVI"
+    assert ar.intToRoman(49)=="XLIX"
+    assert ar.intToRoman(64)=="LXIV"
+    assert ar.intToRoman(81)=="LXXXI"
+    assert ar.intToRoman(100)=="C"
+
