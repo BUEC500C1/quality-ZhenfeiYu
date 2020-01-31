a40c9c70f487b7de6bb8f88daf2ce4e39c74e0f0
ZhenfeiYu <55257370+ZhenfeiYu@users.noreply.github.com>
2020-01-31 15:48:31 -0500

    Update test_arabic2roman.py

--- test_arabic2roman.py
+++ test_arabic2roman.py
@@ -6,8 +6,8 @@ def test_ar():
 	assert ar.arabic2roman(16)=="XVI"
 	assert ar.arabic2roman(25)=="XXV"
 	assert ar.arabic2roman(36)=="XXXVI"
-    assert ar.arabic2roman(49)=="XLIX"
-    assert ar.arabic2roman(64)=="LXIV"
-    assert ar.arabic2roman(81)=="LXXXI"
-    assert ar.arabic2roman(100)=="C"
+    	assert ar.arabic2roman(49)=="XLIX"
+    	assert ar.arabic2roman(64)=="LXIV"
+    	assert ar.arabic2roman(81)=="LXXXI"
+    	assert ar.arabic2roman(100)=="C"
 
