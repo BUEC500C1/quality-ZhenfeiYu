965a506867e353170a8de06deac775271e23fed3
ZhenfeiYu <55257370+ZhenfeiYu@users.noreply.github.com>
2020-05-09 13:11:44 -0400

    Update arabic2roman.py

--- arabic2roman.py
+++ arabic2roman.py
@@ -1,8 +1,16 @@
 def quality(num):
+    if not isinstance(num,int):
+        return "Invalid Input"
     values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
     numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
     res = ""
-    for i, v in enumerate(values):
-        res += (num//v) * numerals[i]
-        num %= v
+    i = 0
+    if num > 0 and num < 5000:
+        while num > 0:
+            for each in range(num//values[i]):
+                res += numerals[i]
+                num -= values[i]
+            i += 1
+    else:
+        return "number should between 0 and 5000"
     return res
