3e72b15cf347d297313cb49c24cd158f960d1e3b
ZhenfeiYu <55257370+ZhenfeiYu@users.noreply.github.com>
2020-05-09 13:12:03 -0400

    Update test_arabic2roman.py

--- test_arabic2roman.py
+++ test_arabic2roman.py
@@ -6,5 +6,8 @@ def test_quality():
 	assert quality(16)=="XVI"
 	assert quality(25)=="XXV"
 	assert quality(36)=="XXXVI"
+	assert quality(5000)=="number should between 0 and 5000"
+	assert quality(-20)=="number should between 0 and 5000"
+	assert quality('one')=="Invalid Input"
 
 
